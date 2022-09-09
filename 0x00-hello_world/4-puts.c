1cf16fc1df82317e22e367d4659b2d1831806a3a
Lesley <tcolesley@gmail.com>
2022-09-08 23:08:19 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- *   
+ *
  * Description: prints "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
@@ -11,5 +11,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
